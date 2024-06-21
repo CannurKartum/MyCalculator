109e5243ba875aa7b296e72452cc6ea2015d0ec2
Cannur Kartum <48458722+CannurKartum@users.noreply.github.com>
2024-06-22 00:05:15 +0200

    coding the basic rules

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.mycalculator
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput: TextView? = null
+    var lastNumeric:Boolean = false
+    var lastDot:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,10 +24,46 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        tvInput = findViewById(R.id.tvInput)
     }
 
     fun onDigit(view: View){
-        Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
     }
+    fun onClear(view: View){
+        tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view:View){
+        if (lastNumeric && !lastDot){
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onOperator(view: View){
+        tvInput?.text?.let {
+            if(lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean{
+        return if (value.startsWith("-")){
+            false
+        }else{
+            value.contains("/")
+                    || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("-")
+        }
+    }
+
 
 }
