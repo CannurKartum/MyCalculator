766bfb866b673e5cdda00635a396be1e57780a68
Cannur Kartum <48458722+CannurKartum@users.noreply.github.com>
2024-06-22 00:58:54 +0200

    finishing the calculations

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
     private var tvInput: TextView? = null
@@ -63,6 +64,63 @@ class MainActivity : AppCompatActivity() {
                     || value.contains("+")
                     || value.contains("-")
         }
+    }
+
+    fun onEqual(view:View){
+        if (lastNumeric){
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+            try{
+                if (tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if (tvValue.contains("-")){
+                    // This is the way how we can see the putted value in the calculator,
+                    // for example if we have "99-1" in the [0] we will get the "99" and in the [1] we will get the "1"
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    removeZero((one.toDouble() - two.toDouble()).toString())
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                }else if (tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    removeZero((one.toDouble() + two.toDouble()).toString())
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                }else if (tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    tvInput?.text = removeZero((one.toDouble() / two.toDouble()).toString())
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                }else if (tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    tvInput?.text = removeZero((one.toDouble() * two.toDouble()).toString())
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                }
+            }catch(e:ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZero(result:String):String{
+        var value = result
+        if (result.contains(".0"))
+            value = result.substring(0, result.length - 2)
+            return value
     }
 
 
